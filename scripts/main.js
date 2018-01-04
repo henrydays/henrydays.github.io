932ffec1377c6ff2205b8e60407923ca668d810a
Henrique <henrymdays@hotmail.com>
2018-01-04 15:02:32 +0000

    responsive nav-bar

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+  $(document).ready(function(){
+    $("#nav-toggle").click(function()
+    {
+		
+	  	
+		 $(".slideshow").css(
+		 {
+		 	"padding-top":"200px"
+		 }
+		 );
+		 $(".slideshow .subtitle").css({
+		 	"top":"400"
+
+		 })
+
+		 $(".slideshow .button").css({
+		 	"top":"400"
+
+		 })
+
+
+    })
+});
